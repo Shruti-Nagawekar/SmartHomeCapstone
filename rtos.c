bb4e33d5fab8ae8e30d0d0d96f580536087553e0
IH8Pink <mcookk900@gmail.com>
2025-11-13 17:16:43 -0600

    TaskAOutline

--- rtos.c
+++ rtos.c
@@ -1,4 +1,28 @@
 // Creating RTOS
+// Timer for 1 kHz tick
+
+#include <stdint.h>
+#include <stdio.h>
+
+static volatile uint32_t tick_count = 0;
+static void (*s_fn)(void) = 0;     // Function pointer to know what function was called
+
+void tick_init(void (*fn)(void)) {
+    s_fn = fn;
+    // Start timer here
+}
+
+uint32_t tick_get(void) {
+    return tick_count;
+}
+
+// When the timer interrupt occurs
+void tick_isr(void) {
+    tick_count++;
+    if (s_fn) {
+        s_fn();
+    }
+}
 
 // Task 1 Sampling Data at 1 kHz
 
--- tasks.c
+++ tasks.c
@@ -1,22 +1,44 @@
 // Code to sample the data points at __ kHz
 #include <stdio.h>
 
+// Need to check if we are storing the average current/voltage/power over time
 typedef struct {
-    int sampling_rate;
-    int state;
-    unsigned char source;  // Determines which INA219 to read from
+    int current;
+    int voltage;
+    int power;
+    int total_current;
+    int total_count;
+    int avg_current;
 } tasksA;
 
 enum A_States {
     INIT,
-    SAMPLE
+    CURRENT0,
+    CURRENT1,
+    VOLTAGE0,
+    VOLTAGE1
 };
 
 int TaskA(int state) {
     switch (state) {
         case INIT:
             break;
-        case SAMPLE:
+        case CURRENT0:
+            // Sample current from first INA219 sensor
+            // Add current to total_current and increment total_count
+            break;
+        case CURRENT1:
+            // Sample current from second INA219 sensor
+            // Add current to total_current and increment total_count
+            break;
+        case VOLTAGE0:
+            // Sample voltage from first INA219 sensor
+            break;
+        case VOLTAGE1:
+            // Sample voltage from second INA219 sensor
             break;
     }
 }
+
+static int readCurrent(int channel, int *output);
+static int readVoltage(int channel, int *output);
